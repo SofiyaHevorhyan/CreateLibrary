ec9a46cd1c858b8149fe5681a992a234d79d84b3
SofiyaHevorhyan <avisventus@gmail.com>
2018-10-25 00:14:47 +0300

    New functiins

--- main.c
+++ main.c
@@ -3,39 +3,37 @@ int read_write(const char* file_read,const char* file_write);
 
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Program starts!\n");
     my_str_t str;
-    my_str_create(&str, 3);
-    my_str_clear(&str);
-    my_str_free(&str);
+    my_str_create(&str, 3);  // initialize with buffer 3
+    my_str_clear(&str);      // clear str
+    my_str_free(&str);       // destroy str
 
     my_str_t str2;
     const char line[] = "symbol";
-    printf("%d\n", my_str_from_cstr(&str2, line, 6));
+    // initialize str from line
+    my_str_from_cstr(&str2, line, 6);
+    printf("Creating str: %s\n", str2.data);
 
-    char* ps = str2.data;
-    while (*ps++ != '\0') {
-        printf("%c ", *(ps-1));
-    }
-    printf("\nsize: %d, buffer: %d, empty?: %d\n",
-            my_str_size(&str2), my_str_capacity(&str2),
+    printf("Size: %d, buffer: %d, empty?: %d\n", my_str_size(&str2), my_str_capacity(&str2),
             my_str_empty(&str2));
 
-    printf("first symbol: %c \n", my_str_getc(&str2, 0));
-    printf("change o to a: ");
-    ps = str2.data;
-    my_str_putc(&str2, 4, 'a');
-    while (*ps++ != '\0') {
-        printf("%c ", *(ps-1));
-    }
-    const char* arr = my_str_get_cstr(&str2);
-    printf("\n%c ", *arr);
-    size_t size = str2.size_m;
-    const char arr1[size];
-    //arr1 = arr;
-    //printf("%c ", *arr1);
+    printf("First symbol: %c \n", my_str_getc(&str2, 0));
+    my_str_putc(&str2, 4, 'm');
+    printf("Change o to m: %s\n", str2.data);
 
-    printf("\n starting test \n");
+    const char* cstr = my_str_get_cstr(&str2);
+    printf("Getting equal c string: %s\n", cstr);
+
+    size_t index = my_str_find_c(&str2, 'm', 3);
+    printf("Find the index of 'm' starts from 3: %d\n", index);
+
+    my_str_t str3;
+    my_str_copy(&str2, &str3, 1);
+    printf("Create a copy of str: %s\n", str3.data);
+
+
+    printf("Starting tests \n");
     printf("\n prev\n");
     my_str_t test_str;
     const char test_line[] = "symbol";
