c71fdc06ed40b8f2819587a79eaefea8ae1b4112
rakus-oksana <oksa.rakus@gmail.com>
2018-10-25 18:36:21 +0300

    adding comments

--- library/lib_for_str.h
+++ library/lib_for_str.h
@@ -62,6 +62,6 @@ size_t my_str_read(my_str_t* str);
 
 int my_str_reorder(my_str_t *str,size_t key_take,size_t key_put);
 
-int my_str_sort(my_str_t* str);
+void my_str_sort(my_str_t* str);
 
 int my_str_read_word(my_str_t *str, FILE *file);
