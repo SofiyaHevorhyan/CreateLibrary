32464d2a823ce80e605c1972a5b7cc6c14f73df6
rakus-oksana <oksa.rakus@gmail.com>
2018-10-25 11:32:26 +0300

    removing prints

--- library/lib_for_str.c
+++ library/lib_for_str.c
@@ -279,8 +279,8 @@ int my_str_read_word(my_str_t *str, FILE *file) {
     if (file != NULL) {
 //      найдовше слово в англ. мові має 45 букв
         char c_str[45];
-        int word_size = fscanf(file, "%s", c_str);        if (word_size < 1) {
-            printf("empty");
+        int word_size = fscanf(file, "%s", c_str);
+        if (word_size < 1) {
             return -1;
         }
         my_str_from_cstr(str, c_str, 0);
